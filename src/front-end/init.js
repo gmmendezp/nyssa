e919a2a412c4d15efee73d2361ba36069ae6bd70
Martin Mendez <gmmendezp@gmail.com>
2016-10-26 20:37:10 -0600

    #1. Improvements to FE init, change to use the correct generator.

--- src/front-end/init.js
+++ src/front-end/init.js
@@ -3,14 +3,14 @@ const exec = require('child_process').exec
 const yeoman = require('yeoman-environment')
 var env = yeoman.createEnv()
 
-module.exports = function (projectName) {
+module.exports = function (projectName, projectPath = '.') {
   console.log(`Generating front end for project ${projectName}...`)
   return new Promise(
     (resolve, reject) => {
       console.log('\nInstalling FE generator...')
       let intervalId = setInterval(
         () => console.log('Still Running... ¯\\_(ツ)_/¯'), 5000)
-      exec('npm i -g generator-webapp', err => {
+      exec('npm i -g generator-nyssa-fe', err => {
         clearInterval(intervalId)
         return err ? reject('There was an error installing the generator') : resolve()
       })
@@ -20,7 +20,13 @@ module.exports = function (projectName) {
       (resolve, reject) => {
         console.log('\nRunning FE generator...')
         env.lookup(
-          () => env.run('webapp', { 'skip-install': true },
-            err => err ? reject(`There was an issue while generating. Code: ${err}`) : resolve()))
+          () => env.run(`nyssa-fe`, {
+            'skip-install': true,
+            name: projectName,
+            path: projectPath
+          }, err => err
+            ? reject(`There was an issue while generating. Code: ${err}`)
+            : resolve()
+          ))
       }))
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 'use strict'
 const program = require('commander')
+const initFE = require('./front-end/init')
 
 program.version('0.0.1')
+
+program
+  .command('start <project-name>').alias('s')
+  .description('Start a project in a new folder')
+  .action(function (projectName) {
+    initFE(projectName, `fe/${projectName}`).catch(console.error)
+    // initBE(projectName, `be/${projectPath}`)
+  })
 
 program
   .command('init <project-name>').alias('i')
   .description('Initialize a project in the current folder')
   .action(function (projectName) {
-    require('./front-end/init')(projectName).catch(console.error)
-    // require('./back-end/init')()
+    initFE(projectName, 'fe').catch(console.error)
+    // initBE(projectName, 'be')
   })
 
 program
